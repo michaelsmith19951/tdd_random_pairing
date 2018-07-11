971db1e0849fdb1cb84094560bb199c704957858
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-11 16:00:12 -0400

    13th test that failed, test_counting_arrays

--- tdd_random_pairing.rb
+++ tdd_random_pairing.rb
@@ -1,15 +1,24 @@
 # Function that sets a variable equal to all 5 names, which are in an array
-def arr()
-	people = ["Tom", "Dick", "Harry", "Jenny", "Sally"]
-# shuffled_arr[]
-# shuffled_arr[] = people.shuffle
-# first = []
-# second = []
+def shuffled_names(people)
+	# people = ["Tom", "Dick", "Harry", "Jenny", "Sally"]
+shuffled_arr = []
+shuffled_arr = people.shuffle
+p shuffled_arr
+first = []
+results = []
+i = 0
 
-# i = 0
-# until first.length > shuffled_arr.length
-# 	first[i] << shuffled_arr.pop
-# end	
-# Print the randomized list of names to the screen
-# p people
-end	
+until first.count == 3
+	p" #{first.length} first array length"
+	p" #{shuffled_arr.length} shuffled array length"
+	p first
+	first << shuffled_arr.pop
+	i += 1
+	end
+
+	results << first
+	p" shuffled_arr #{shuffled_arr}"
+	results << shuffled_arr
+	p results
+	results
+end
--- test_tdd_random_pairing.rb
+++ test_tdd_random_pairing.rb
@@ -7,27 +7,43 @@ require_relative "tdd_random_pairing.rb"
 # Test the test_new_calc.rb for passes and failures
 class TestTddRandomPairing < Minitest::Test
 
-def test_Tom_0
-	assert_equal("Tom", arr()[0])
-end	
+# def test_Tom_0
+# 	assert_equal("Tom", arr()[0])
+# end	
 
-def test_Dick_1
-	assert_equal("Dick", arr()[1])
-end	
+# def test_Dick_1
+# 	assert_equal("Dick", arr()[1])
+# end	
 
-def test_Harry_2
-	assert_equal("Harry", arr()[2])
-end
+# def test_Harry_2
+# 	assert_equal("Harry", arr()[2])
+# end
 
-def test_Jenny_3
-	assert_equal("Jenny", arr()[3])
-end
+# def test_Jenny_3
+# 	assert_equal("Jenny", arr()[3])
+# end
 
-def test_Sally_4
-	assert_equal("Sally", arr()[4])
-end
+# def test_Sally_4
+# 	assert_equal("Sally", arr()[4])
+# end
 
 def test_if_array_is_class
-	assert_equal(Array, arr().class)
+	people = ["Tom", "Dick", "Harry", "Jenny", "Sally"]
+	assert_equal(Array, shuffled_names(people).class)
+end
+
+# def test_if_shuffled_array_has_correct_length
+# 	people = ["Tom", "Dick", "Harry", "Jenny", "Sally"]
+# 	assert_equal(5, shuffled_names(people).count)
+# end
+
+# def test_if_first_array_has_correct_length
+# 	people = ["Tom", "Dick", "Harry", "Jenny", "Sally"]
+# 	assert_equal(5, shuffled_names(people).count)
+
+def test_counting_arrays
+	people = ["Tom", "Dick", "Harry", "Jenny", "Sally"]
+	results = shuffled_names(people)
+	assert_equal(3, results.count)
 end
 end
